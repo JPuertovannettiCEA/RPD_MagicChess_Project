52319928d22f23cd1c8dbabaa4951effb073eb2c
arvind <gd04arvind@thecea.ca>
2021-11-24 21:43:16 -0800

    Basic Movement of Healer Done

--- Assets/Scripts/King.cs
+++ Assets/Scripts/King.cs
@@ -6,7 +6,7 @@ public class King : Chessman
     public override bool[,] PossibleMoves()
     {
         bool[,] r = new bool[8, 8];
-        Debug.Log($"KingHuMai");
+        // Debug.Log($"KingHuMai");
         Move(CurrentX + 1, CurrentY, ref r); // up
         Move(CurrentX - 1, CurrentY, ref r); // down
         Move(CurrentX, CurrentY - 1, ref r); // left
@@ -18,7 +18,5 @@ public class King : Chessman
 
         return r;
     }
-
-
 
 }
